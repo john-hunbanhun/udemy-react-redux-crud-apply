9b0d93368981d166fcdd553db189d07433a6977f
ando.yuma <foymitsu-1115pye@outlook.jp>
2020-04-29 11:11:20 +0900

    EventsNewの入力フォームの実装(reduxForm使用)

--- udemy-react-redux-crud-apply/src/components/eventsnew.js
+++ udemy-react-redux-crud-apply/src/components/eventsnew.js
@@ -1,20 +1,71 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import { postEvent } from "../Actions";
+import { postEvent } from "../Actions";
+import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 
 class EventsNew extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error },
+    } = field;
+    console.log(field);
     return (
-      <React.Fragment>
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+
+  async onSubmit(values) {
+    await this.props.postEvent(values);
+    this.props.history.push("/");
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
-          <p>foo</p>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          />
         </div>
-        <Link to="/">Home</Link>
-      </React.Fragment>
+        <div>
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          />
+          <div>
+            <input type="submit" value="submit" disabled={false} />
+            <Link to="/">Home</Link>
+          </div>
+        </div>
+      </form>
     );
   }
 }
-// const mapDispatchProps = { postEvents };
-
-export default connect(null, null)(EventsNew);
+const mapDispatchProps = { postEvent };
+const validate = (values) => {
+  const errors = {};
+  if (!values.title) errors.title = "Enter Title";
+  if (!values.body) errors.body = "Enter body";
+  return errors;
+};
+export default connect(
+  null,
+  mapDispatchProps
+)(reduxForm({ validate: validate, form: "eventNewform" })(EventsNew));
